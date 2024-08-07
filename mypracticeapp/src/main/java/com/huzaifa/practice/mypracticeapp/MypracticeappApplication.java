c13dfc2867fcf53407cdcbf8cbdc31c7d39b772e
MHUZAIFA <mhuzaifa.career@outlook.com>
2024-08-07 12:34:47 +0530

    initialized dev spring boot project using spring initializr - https://start.spring.io/

---
+++ mypracticeapp/src/main/java/com/huzaifa/practice/mypracticeapp/MypracticeappApplication.java
@@ -0,0 +1,13 @@
+package com.huzaifa.practice.mypracticeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypracticeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypracticeappApplication.class, args);
+	}
+
+}
---
+++ mypracticeapp/src/test/java/com/huzaifa/practice/mypracticeapp/MypracticeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huzaifa.practice.mypracticeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypracticeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
