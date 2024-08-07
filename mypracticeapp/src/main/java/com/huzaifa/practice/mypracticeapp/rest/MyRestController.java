83f7193b272121c307ade77a3645a9f1480c378c
MHUZAIFA <mhuzaifa.career@outlook.com>
2024-08-07 13:09:31 +0530

    application.properties usage
    - Updated server port number to 8585
    - Read values from properties file

--- mypracticeapp/src/main/java/com/huzaifa/practice/mypracticeapp/rest/MyRestController.java
+++ mypracticeapp/src/main/java/com/huzaifa/practice/mypracticeapp/rest/MyRestController.java
@@ -1,13 +1,28 @@
 package com.huzaifa.practice.mypracticeapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+
+    @Value("${message.hello_world_title}")
+    private String title;
+
+    @Value("${message.hello_world_description}")
+    private String description;
+
+    @Value("${message.hello_world_disclaimer}")
+    private String disclaimer;
+
     // expose "/" that returns "Hello World"
     @GetMapping("/")
     public String SayHelloWorld() {
-        return "<h1>Hello World!!!</h1>";
+        return "<div style=\"text-align: center\">"
+                + "<h1>" + title + "</h1>"
+                + "<p>" + description + "</p>"
+                + disclaimer +
+                "</div>";
     }
 }
