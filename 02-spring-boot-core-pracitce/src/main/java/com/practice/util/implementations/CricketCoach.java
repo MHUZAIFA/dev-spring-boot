a3f8fa54790c55d861b2a8e96a2262946d32a1d7
MHUZAIFA <mhuzaifa.career@outlook.com>
2024-08-14 17:23:33 +0530

    Bean - Prototype Scope Demo
    - Used @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) to enable prototype scope.
    - Prototype scope creates a new instance for each bean initialization.

--- 02-spring-boot-core-pracitce/src/main/java/com/practice/util/implementations/CricketCoach.java
+++ 02-spring-boot-core-pracitce/src/main/java/com/practice/util/implementations/CricketCoach.java
@@ -1,9 +1,12 @@
 package com.practice.util.implementations;
 
 import com.practice.util.interfaces.ICoach;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements ICoach {
 
     public CricketCoach() {
