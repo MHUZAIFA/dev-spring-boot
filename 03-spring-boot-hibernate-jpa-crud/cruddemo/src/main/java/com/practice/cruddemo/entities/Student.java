ea40c41799241f81f896afe80ecaff3dba8b85d7
MHUZAIFA <mhuzaifa.career@outlook.com>
2024-08-16 00:54:58 +0530

    Entity boiler plate
    student entity boiler plate

---
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/practice/cruddemo/entities/Student.java
@@ -0,0 +1,18 @@
+package com.practice.cruddemo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    // define fields
+
+    // define constructors
+
+    // define getter/setters
+
+    // define toString() method
+
+}
